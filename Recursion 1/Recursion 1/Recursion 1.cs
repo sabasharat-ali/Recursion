9d146cca4016571a4381fb915faf870de78b7116
Syed Ahmed Basharat Ali <57292228+sabasharat-ali@users.noreply.github.com>
2021-03-15 00:12:48 +0500

    Add files via upload

---
+++ Recursion 1/Recursion 1/Recursion 1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Recursion_1
+{
+    //A program that calculates the sum of the numbers from "n" to "m"
+    class Program
+    {
+        public static int CalculateSumRecursively(int n, int m)
+        {
+            int sum = n;
+
+            if (n < m)
+            {
+                n++;
+                return sum += CalculateSumRecursively(n, m);
+            }
+
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter number m: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            int sum = CalculateSumRecursively(n, m);
+
+            Console.WriteLine(sum);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Recursion 2/Recursion 2/Recursion 2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Recursion_2
+{
+    //Program to print factorials recursively
+    class Factorial
+    {
+
+        public int factR(int n)
+        {
+            int result;
+
+            if (n == 1) return 1;
+            result = factR(n - 1) * n;
+            return result;
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+               
+                Factorial f = new Factorial();
+                Console.WriteLine("Factorial of 3 is " + f.factR(3));
+                Console.WriteLine("Factorial of 4 is " + f.factR(4));
+                Console.WriteLine("Factorial of 5 is " + f.factR(5));
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
